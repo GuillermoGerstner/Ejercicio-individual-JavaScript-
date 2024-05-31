d4cffc142009cd827c7a006912da6c9ca8d5bd3f
Guillermo Gerstner <guillermogerstner@gmail.com>
2024-05-31 03:08:18 -0300

    first commit

---
+++ Gerstner_1189563_15052024_JavaScript.js
@@ -0,0 +1,8 @@
+var nombre = prompt("Ingrese su nombre:");
+var apellido = prompt("Ingrese su apellido:");
+var edad = prompt("Ingrese su edad:");
+
+alert(
+  "Usted es " + nombre + " " + apellido + " y su edad es " + edad + " años."
+);
+alert("Desde que nació ha vivido " + edad * 365 + " días.");
